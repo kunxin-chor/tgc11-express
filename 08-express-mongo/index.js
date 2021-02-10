594ed2f0bbc30369a48415a53f2dcc4030b54ae1
kunxin-chor <chorkunxin@yahoo.com>
2021-02-10 08:18:35 +0000

    Added sample recipie doc

--- 08-express-mongo/index.js
+++ 08-express-mongo/index.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const hbs = require("hbs");
 const wax = require("wax-on");
-const mongoClient = require("mongodb").MongoClient;
 require('dotenv').config();
 const mongoUrl = process.env.MONGO_URL;
 const MongoUtil = require('./MongoUtil');
 
+// create an instance of express app
 let app = express();
 
+// set the view engine
+app.set('view engine', 'hbs');
+
+// static folder
+app.use(express.static('public'))
+
+// setup wax-on
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts');
+
+// enable forms
+app.use(express.urlencoded({
+    'extended':false
+}));
+
 async function main() {
-    let db = await MongoUtil.connect(mongoUrl, 'tgc11_movie_app')
+    let db = await MongoUtil.connect(mongoUrl, 'tgc11_recipes')
+
+    // MongoDB is connected and alive
+
+    app.get('/')
 }   
 
 main();
