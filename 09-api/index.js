082135f7296d3ff660508987b38cde5b9c10a61d
kunxin-chor <chorkunxin@yahoo.com>
2021-02-15 07:23:35 +0000

    Added API example

--- 09-api/index.js
+++ 09-api/index.js
@@ -1,15 +1,8 @@
 // SETUP BEGINS
 const express = require("express");
-const hbs = require("hbs");
-const wax = require("wax-on");
-const axios = require("axios");
-
+const cors = require('cors');
 let app = express();
-app.set("view engine", "hbs"); // tell express to use hbs as the view engine
-app.use(express.static("public")); // tell express where to find static files (css, json,  geojson, image files, js)
 
-wax.on(hbs.handlebars); // for template inheritance
-wax.setLayoutPath("./views/layouts"); // tell wax-on where to find the layout file
 
 // !! Enable forms processing
 app.use(
@@ -18,12 +11,27 @@ app.use(
   })
 );
 
+// !! Enable processing JSON data
+app.use(express.json());
+
+// !! Enable CORS
+app.use(cors());
+
 // SETUP END
 app.get('/api/greetings',(req, res)=>{
+    a.b();
     // If you send back a JavaScript object, Express will automatically change
     // it into JSON for you!
     res.send({
         'message':'Hello world'
+    })
+})
+
+app.post('/api/sayhello', (req,res)=>{
+    let fname = req.body.firstName;
+    let lname = req.body.lastName;
+    res.send({
+        'message':"Hello there " + fname + " " + lname
     })
 })
 
