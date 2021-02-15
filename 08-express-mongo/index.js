5b6a7e0ee83e03d3df5d079450a616908c63b736
kunxin-chor <chorkunxin@yahoo.com>
2021-02-15 02:34:55 +0000

    Demonstrate how to do perform pulls

--- 08-express-mongo/index.js
+++ 08-express-mongo/index.js
@@ -29,6 +29,13 @@ app.use(
 async function main() {
   let db = await MongoUtil.connect(mongoUrl, "tgc11_recipes");
 
+  async function getIngredientById(ingredientId) {
+    let ingredient = await db.collection("ingredients").findOne({
+      _id: ObjectId(ingredientId)
+    });
+    return ingredient;
+  }
+
   // MongoDB is connected and alive
 
   // Show the form to create the ingredient
@@ -42,7 +49,7 @@ async function main() {
       name: req.body.ingredientName
     });
 
-    res.redirect('/ingredients')
+    res.redirect("/ingredients");
   });
 
   // Show all ingredients in the system
@@ -53,58 +60,115 @@ async function main() {
       .find({}) // find all the ingredient with no criteria
       .toArray(); // convert to array
 
-    res.render('ingredients/all', {
-        'everything': ingredients
-    })
+    res.render("ingredients/all", {
+      everything: ingredients
+    });
   });
 
   // Delete ingredient from the system
-  app.get('/ingredients/:ingredient_id/delete', async (req,res)=>{
+  app.get("/ingredients/:ingredient_id/delete", async (req, res) => {
     let id = req.params.ingredient_id;
-    let ingredient = await db.collection('ingredients').findOne({
-        '_id':ObjectId(id)
-    })
+    let ingredient = await getIngredientById(ingredient_id);
+
     // test to ensure it's working
-    res.render('ingredients/delete',{
-        'ingredient':ingredient
-    })
-  })
+    res.render("ingredients/delete", {
+      ingredient: ingredient
+    });
+  });
 
   // process is what sent via the form
-  app.post('/ingredients/:ingredient_id/delete', async(req,res)=>{
-        await db.collection('ingredients').remove({
-            '_id':ObjectId(req.params.ingredient_id)
-        })
-        res.redirect('/ingredients')
-  })
+  app.post("/ingredients/:ingredient_id/delete", async (req, res) => {
+    await db.collection("ingredients").remove({
+      _id: ObjectId(req.params.ingredient_id)
+    });
+    res.redirect("/ingredients");
+  });
 
   // update
-  app.get('/ingredients/:ingredient_id/update', async (req,res)=>{
-      // we retrieve the ingredient information
-      let ingredient_id = req.params.ingredient_id;
-      let ingredient = await db.collection('ingredients').findOne({
-          '_id': ObjectId(ingredient_id)
-      });
+  app.get("/ingredients/:ingredient_id/update", async (req, res) => {
+    // we retrieve the ingredient information
+    let ingredient_id = req.params.ingredient_id;
+    let ingredient = await getIngredientById(ingredient_id);
 
-      res.render('ingredients/update', {
-          'ingredient': ingredient
-      })
-  })
+    res.render("ingredients/update", {
+      ingredient: ingredient
+    });
+  });
 
-  app.post('/ingredients/:ingredient_id/update',async (req,res)=>{
-      let newIngredientName = req.body.ingredientName;
-      let ingredientId = req.params.ingredient_id;
-      db.collection('ingredients').updateOne({
-          '_id': ObjectId(ingredientId)
-      }, {
-          '$set': {
-            'name': newIngredientName
-          }           
-      });
+  app.post("/ingredients/:ingredient_id/update", async (req, res) => {
+    let newIngredientName = req.body.ingredientName;
+    let ingredientId = req.params.ingredient_id;
+    db.collection("ingredients").updateOne(
+      {
+        _id: ObjectId(ingredientId)
+      },
+      {
+        $set: {
+          name: newIngredientName
+        }
+      }
+    );
 
-      res.redirect('/ingredients')
-  })
+    res.redirect("/ingredients");
+  });
 
+  // add a new comment to an ingredient
+  app.get("/ingredients/:ingredient_id/comments/create", async (req, res) => {
+    let ingredient = await getIngredientById(req.params.ingredient_id);
+    res.render("ingredients/add_comments", {
+      ingredient: ingredient
+    });
+  });
+
+  app.post("/ingredients/:ingredient_id/comments/create", async (req, res) => {
+    // add the new comment as an ELEMENT in the comments array
+    //   let { email, comments} = req.body;
+    let email = req.body.email;
+    let comments = req.body.comments;
+    let ingredientID = req.params.ingredient_id;
+
+    await db.collection("ingredients").updateOne(
+      {
+        _id: ObjectId(ingredientID)
+      },
+      {
+        $push: {
+          comments: {
+            _id: ObjectId(), // ask Mongo to generate an ID for us
+            email: email,
+            comments: comments
+          }
+        }
+      }
+    );
+    res.redirect("/ingredients");
+  }); // end app.post
+
+  // show comments for an ingredient
+  app.get("/ingredients/:ingredient_id/comments", async (req, res) => {
+    let ingredient = await getIngredientById(req.params.ingredient_id);
+    res.render("ingredients/show", {
+      ingredient: ingredient
+    });
+  }); // end show comments
+
+  app.get(
+    "/ingredients/:ingredient_id/comments/:comment_id/delete",
+    async (req, res) => {
+        console.log(req.params);
+      await db.collection("ingredients").updateOne(
+        {
+          _id: ObjectId(req.params.ingredient_id)
+        },
+        {
+          '$pull': {
+            'comments': {'_id':ObjectId(req.params.comment_id)}
+          }
+        }
+      );
+      res.send("comment has been deleted");
+    }
+  );
 }
 
 main();
