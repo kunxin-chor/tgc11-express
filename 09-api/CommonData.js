1f814276ba5be27a1dbdf963804742e0c469a841
kunxin-chor <chorkunxin@yahoo.com>
2021-02-15 08:00:54 +0000

    Added basic api example

---
+++ 09-api/CommonData.js
@@ -0,0 +1,8 @@
+const CommonData = {
+    'PI': 3.14,
+    'SECRET_OF_LIFE': 42,
+    'ROSEBUD':"is his sled",
+    'letter':"Dear <name>, your bill of <bill> is overdue"
+}
+
+module.exports = CommonData;
--- 09-api/index.js
+++ 09-api/index.js
@@ -1,8 +1,8 @@
 // SETUP BEGINS
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const CommonData = require("./CommonData");
 let app = express();
-
 
 // !! Enable forms processing
 app.use(
@@ -18,22 +18,32 @@ app.use(express.json());
 app.use(cors());
 
 // SETUP END
-app.get('/api/greetings',(req, res)=>{
-    a.b();
-    // If you send back a JavaScript object, Express will automatically change
-    // it into JSON for you!
-    res.send({
-        'message':'Hello world'
-    })
-})
+app.get("/api/greetings", (req, res) => {
+  // If you send back a JavaScript object, Express will automatically change
+  // it into JSON for you!
+  res.send({
+    message: "Hello world"
+});
 
-app.post('/api/sayhello', (req,res)=>{
-    let fname = req.body.firstName;
-    let lname = req.body.lastName;
-    res.send({
-        'message':"Hello there " + fname + " " + lname
-    })
-})
+app.post("/api/sayhello", (req, res) => {
+  let fname = req.body.firstName;
+  let lname = req.body.lastName;
+  res.send({
+    message: "Hello there " + fname + " " + lname
+  });
+
+  // for registration
+  // we focus on the end results of a registration
+  // http://www.myapi.com/api/query?searchTerms=IBM&apiKey=1234567
+  app.get('api/query',(req,res)=>{
+      let searchTerms = req.query.searchTerms;
+      let apiKey = req.query.apiKey
+      // do my search in Mongo
+      // return as JSON
+  })
+
+});
+
 
 // START SERVER
 app.listen(3000, () => {
